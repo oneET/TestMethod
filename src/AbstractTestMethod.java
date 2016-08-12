ea3fb13e789d7803eb8a0dbb414ae3ec07ebe1c7
oneET <oneET@users.noreply.github.com>
2016-08-12 09:30:09 +0800

    Create AbstractTestMethod.java
    create abstract testMethod

---
+++ src/AbstractTestMethod.java
@@ -0,0 +1,12 @@
+/**
+ * 定义抽象测试方法，所有的测试类都要继承改抽象类
+ */
+public abstract class AbstractTestMethod {
+    //定义URL地址的共同部分
+    protected String String url = "localhost:8081/AProject";
+    
+    //按链式结构，定义URL的地址
+    protected URIBuilder URIBuilder(String path) {
+  		return new URIBuilder().setScheme("http").setHost(url + path);
+  	}
+}
